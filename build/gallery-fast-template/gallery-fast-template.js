a9ea64b6d2ad715e71354fbee1f77147c4c322ba
David Martin <drmartin1998@gmail.com>
2011-02-14 22:00:26 -0600

    Added gallery-fast-template

---
+++ build/gallery-fast-template/gallery-fast-template.js
@@ -0,0 +1,33 @@
+YUI.add('gallery-fast-template', function(Y) {
+
+
+		function fastTemplate( config) 
+		{
+	 		fastTemplate.superclass.constructor.apply( this, arguments );
+		}
+		
+		fastTemplate.NAME = "fastTemplate";
+		
+		fastTemplate.ATTRS = {
+			data : {},
+			variables: {},
+			templateNode: {},
+			wrapperNode: {}
+		};
+		
+		Y.extend( fastTemplate, Y.Base, {
+			process : function() {
+				var content = '';
+				var myData = this.get('data');
+				var htmlSnip = this.get('templateNode').get('innerHTML');
+				Y.each(myData, function(i){
+					content += Y.substitute( htmlSnip, i);	
+				}, this);
+				this.get('wrapperNode').set('innerHTML', content);
+			}
+		});
+		
+		Y.fastTemplate = fastTemplate;
+
+
+}, '@VERSION@' ,{requires:['node', 'base', 'substitute']});
